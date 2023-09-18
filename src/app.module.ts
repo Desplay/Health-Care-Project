9c064bb7dc07acfb37b70f4f16fc9ca32428e66f
Desplay Shido <desplayshido@gmail.com>
2023-09-18 08:22:31 +0700

    update basic model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LobbyModule } from './lobby/lobby.module';
+import { ReceptionModule } from './reception/reception.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [],
+  imports: [LobbyModule, ReceptionModule, DoctorModule, PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
